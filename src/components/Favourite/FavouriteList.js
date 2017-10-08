a640452d751ca71fcd214e41adbd8a6b482c6891
phiwanczuk <phiwanczuk@gmail.com>
2017-10-08 22:22:56 +0200

    'added_main_list_and_form'

--- src/components/Favourite/FavouriteList.js
+++ src/components/Favourite/FavouriteList.js
@@ -1,3 +1,32 @@
 import React from 'react'
 
 import Favourite
+
+class FavouriteList extends React.Component {
+  render() {
+    const { searches } = this.props
+
+    return (
+
+      <div>
+        <SearchForm/>
+
+        <ul>
+
+          {
+
+            search && searches.map(
+              ({id, name}, index, allSearches) => (
+                <li key={id}>{name}</li>
+              )
+            )
+
+          }
+        </ul>
+      </div>
+
+    )
+  }
+}
+
+export default SearchList
