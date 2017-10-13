0592ed9602be249ba10291f5f0a40f8cde46f2d4
jotem1st <jakub@jotem.in>
2017-10-13 16:31:44 +0200

    added single item view

--- src/components/Search/SingleView.js
+++ src/components/Search/SingleView.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Alert, Button, ButtonGroup, ButtonToolbar, Table} from 'react-bootstrap'
 
 
 class SingleView extends React.Component{
@@ -9,14 +11,22 @@ class SingleView extends React.Component{
       {
         this.props.filteredProduct ?
           <div>
-            <h2> Aktualnie przeglądasz: &nbsp;
-              {this.props.filteredProduct.name}
+            <h2> {this.props.filteredProduct.name}
             </h2>
-            <h3>Przepis dodany w kategorii: {this.props.mealCategory}
-            </h3>
+            <h4>Kategoria: {this.props.filteredProduct.category}
+            </h4>
+            <h4>Liczba kalorii: {this.props.filteredProduct.kcal}</h4>
+            <Button href="#">dodaj do ulubionych</Button>
+            <div><img src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/> </div>
             <p>
-              Szczegóły przepisu: {this.props.mealRecipe}
+              Szczegóły przepisu: {this.props.filteredProduct.recipe}
             </p>
+
+{/*button do przodu i do tyłlu*/}
+
+            <Button href="#" >poprzedni</Button>
+            <Button href="#">następny</Button>
+
           </div>
           :
           'Ładowanie'
