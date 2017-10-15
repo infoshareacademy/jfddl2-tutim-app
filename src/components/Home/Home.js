83ad25841afa3d1dfd95cea46f8d7f83ebc39d55
Piotrek <phiwanczuk@gmail.com>
2017-10-15 20:07:29 +0200

    welcome page styles added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,12 @@ class Home extends React.Component {
         return (
             <div>
                 <h1 class="welcomePI">Witaj na FitStyle!</h1>
-                <h2 class="yoursPI">Twoim spersonalizowanym planerze posiłków!</h2>
+                <div class="firstPI">
+                <p class="firstPI">Jeśli jesteś osobą, która prowadzi zdrowy tryb życia, a przy tym ceni swój czas, to jesteś w idealnym miejscu!
+                Nasza aplikacja pozwoli Ci gromadzić Twoje przepisy!  Dzięki szerokiej funkcjonalności, juz teraz masz możliwość dodawania nieograniczonej liczby składników, które pózniej zamienisz na przepyszne, zdrowe potrawy! Na start dostajesz od nas dostęp do naszej bazy przepisów, by pokazać Ci jakie to proste!
+                </p>
+                </div>
 
-                <p class="healthyPI">
-                    Zdrowe odżywianie &ne; życie na liściu sałaty!</p>
                     <p class="rightsreservedPI">TuTim&#169;</p>
             </div>
         )
