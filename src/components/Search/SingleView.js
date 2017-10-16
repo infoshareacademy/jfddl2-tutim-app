c0d02a164b13ed17bea161a01b79b19b8c74e71b
jotem1st <jakub@jotem.in>
2017-10-16 10:46:39 +0200

    fixed font in search table; fixed single view css

--- src/components/Search/SingleView.js
+++ src/components/Search/SingleView.js
@@ -1,36 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Alert, Button, ButtonGroup, ButtonToolbar, Table} from 'react-bootstrap'
+import styles from './jakub.css';
 
-
-class SingleView extends React.Component{
-  render (){
+class SingleView extends React.Component {
+  render() {
     console.log(this.props.filteredProduct)
-    return(
+    return (
       <div>
-      {
-        this.props.filteredProduct ?
-          <div>
-            <h2> {this.props.filteredProduct.name}
-            </h2>
-            <h4>Kategoria: {this.props.filteredProduct.category}
-            </h4>
-            <h4>Liczba kalorii: {this.props.filteredProduct.kcal}</h4>
-            <Button href="#">dodaj do ulubionych</Button>
-            <div><img src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/> </div>
-            <p>
-              Szczegóły przepisu: {this.props.filteredProduct.recipe}
-            </p>
+        {
+          this.props.filteredProduct ?
+            <div className="singleView">
+              <h2> {this.props.filteredProduct.name}
+              </h2>     <div className="addToFav likeMe"> <a href="#">dodaj do ulubionych</a></div>
 
-{/*button do przodu i do tyłlu*/}
+              <div className="heading">
+                <h4>Kategoria: {this.props.filteredProduct.category}
+                </h4>
+                <h4>Liczba kalorii: {this.props.filteredProduct.kcal}</h4>
+              </div>
 
-            <Button href="#" >poprzedni</Button>
-            <Button href="#">następny</Button>
+              <div><img
+                src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/>
+              </div>
+             <div className="description"> <p>
+               <b>Szczegóły przepisu:</b> &nbsp; {this.props.filteredProduct.recipe}
+              </p></div>
 
-          </div>
-          :
-          'Ładowanie'
-      }
+              {/*button do przodu i do tyłlu*/}
+
+              <div className="PrevNext"> <a href="#">poprzedni</a>
+              <a href="#">następny</a></div>
+
+            </div>
+            :
+            'Ładowanie'
+        }
       </div>
 
 
