3d6e73c36953b19f4160fb3b9a8fbaaafc2826e2
phiwanczuk <phiwanczuk@gmail.com>
2017-10-25 15:22:32 +0200

    planer to redux

--- src/state/planer.js
+++ src/state/planer.js
@@ -1,1 +1,18 @@
-const
+import { database } from "../firebase";
+
+
+const ADD_PLAN = 'planer/ADD_PLAN'
+
+const addPlan = plan => ({
+  plan: ADD_PLAN
+})
+
+
+export const addPlan = plan => () => {
+  const id = planer().currentData.id
+  database().ref('/planer' + id + '/planer').push({
+    category: category,
+
+
+  })
+}
