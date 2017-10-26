7bc9b472d2e1dc258a0323599564fc5df41d21d1
phiwanczuk <phiwanczuk@gmail.com>
2017-10-26 18:43:02 +0200

    WIP

--- src/components/Planer/PlanerTable.js
+++ src/components/Planer/PlanerTable.js
@@ -1,37 +1,102 @@
 import React from 'react'
 import {Table} from 'react-bootstrap'
 
+import {connect} from 'react-redux'
+
+import BigCalendar from 'react-big-calendar'
+import events from './events'
+
+
+//
+// class PlanerTable extends React.Component{
+//   render() {
+//     return (
+//       <div>
+//         <h3 className="callout">
+//           Click an event to see more info, or
+//           drag the mouse over the calendar to select a date/time range.
+//         </h3>
+//         <BigCalendar
+//           selectable
+//           events={events}
+//           defaultView='week'
+//           scrollToTime={new Date(1970, 1, 1, 6)}
+//           defaultDate={new Date(2015, 3, 12)}
+//           onSelectEvent={event => alert(event.title)}
+//           // onSelectSlot={(slotInfo) => alert(
+//           //   `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+//           //   `\nend: ${slotInfo.end.toLocaleString()}`
+//           // )}
+//         />
+//       </div>
+//     )
+//   }
+//
+// }
+
 
 class PlanerTable extends React.Component {
   render() {
+    const getPlanForMealAndDay = (meal, day) => this.props.planerData.filter((recipe) => recipe.meal === meal && recipe.day === day)
+
+    const renderRecipes = (arrayOfRecipes) => arrayOfRecipes.map((recipe) => <div>{recipe.uid}</div>)
+
+    console.log(renderRecipes(getPlanForMealAndDay(1, 2)))
+
     return (
 
       <Table striped bordered condensed hover>
         <thead>
         <tr>
-          <th>#</th>
-          <th>Pora dnia</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Posiłek</th>
+          <th>Pon</th>
+          <th>Wt</th>
+          <th>Śr</th>
+          <th>Czw</th>
+          <th>Pt</th>
+          <th>Sob</th>
+          <th>Sob</th>
+          <th>Nd</th>
         </tr>
         </thead>
         <tbody>
         <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td>Śniadanie</td>
+          <td>
+          </td>
+          <td>
+
+          </td>
+          <td>
+            {renderRecipes(getPlanForMealAndDay(1, 2))}
+          </td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
         <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+          <td>Śniadanie</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
         <tr>
-          <td>3</td>
-          <td colSpan="2">Larry the Bird</td>
-          <td>@twitter</td>
+          <td>Śniadanie</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
         </tbody>
       </Table>
@@ -39,4 +104,10 @@ class PlanerTable extends React.Component {
   }
 }
 
-export default PlanerTable
+const mapStateToProps = state => ({
+  planerData: state.planer.plan
+})
+
+export default connect(
+  mapStateToProps
+)(PlanerTable)
