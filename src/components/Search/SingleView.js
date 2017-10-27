6c5eaf904b272d1f07ad272a52ec5549b73b313e
phiwanczuk <phiwanczuk@gmail.com>
2017-10-27 15:17:24 +0200

    single view 27

--- src/components/Search/SingleView.js
+++ src/components/Search/SingleView.js
@@ -25,70 +25,59 @@ class SingleView extends React.Component {
 
   render() {
     return (
-
-
-
       <div>
         {
-          this.props.filteredProduct ?
-            <div className="singleView">
+          this.props.filteredProduct
+            ? <div className="singleView">
               <h2> {this.props.filteredProduct.name}
               </h2>
               <div className="addToFav likeMe">
                 <button><a href="#" onClick={() => this.addToFavourites(this.props.filteredProduct.uid)}
-                           class="linker">dodaj do ulubionych</a></button>
+                           className="linker">dodaj do ulubionych</a></button>
               </div>
 
               <div className="heading">
-                <h4>Kategoria: {this.props.filteredProduct.category}
-                </h4>
+                <h4>Kategoria: {this.props.filteredProduct.category}</h4>
                 <h4>Liczba kalorii: {this.props.filteredProduct.kcal}</h4>
                 <h4>Składniki odżywcze w 100 g produktu:</h4>
                 <h4>Białka: {this.props.filteredProduct.containsIn100g.protein}</h4>
                 <h4>Tłuszcze: {this.props.filteredProduct.containsIn100g.fat}</h4>
                 <h4>Węglowodany: {this.props.filteredProduct.containsIn100g.carbs}</h4>
                 <img src={this.props.filteredProduct.image}/>
-    </div>
-
-
-{/*<PieChartWithCustomizedLabel/>*/}
-
-              {/*<div><img*/}
-                {/*src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/>*/}
-              {/*</div>*/}
-             <div className="description"> <p>
-               <b>Szczegóły przepisu:</b> &nbsp; {
-               this.props.filteredProduct.recipe.split('\n').map(
-                 row => <p>{row}</p>
-               )
-             }
-
-              <div><img
-                src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/>
               </div>
-              <div className="description"><p>
-                <b>Szczegóły przepisu:</b> &nbsp; {
-                this.props.filteredProduct.recipe.split('\n').map(
-                  row => <p>{row}</p>
-                )
-              }
 
-              </p></div>
+              <div className="description">
+                <p>
+                  <b>Szczegóły przepisu:</b> &nbsp; {
+                  this.props.filteredProduct.recipe.split('\n').map(
+                    row => <p>{row}</p>
+                  )
+                }
+                </p>
 
-              {/*button do przodu i do tyłlu*/}
+                <div><img
+                  src="https://www.foodlion.com/content/dam/food-lion/recipes/Citrus%20Glazed%20Spiral%20Cut%20Ham/citrusspiralcutham_main.jpg.adapt.full.high.jpg"/>
+                </div>
+                <div className="description"><p>
+                  <b>Szczegóły przepisu:</b> &nbsp; {
+                  this.props.filteredProduct.recipe.split('\n').map(
+                    row => <p>{row}</p>
+                  )
+                }
 
-              <div className="PrevNext"><a href="#">poprzedni</a>
-                <a href="#">następny</a></div>
+                </p>
+                </div>
 
+                <div className="PrevNext">
+                  <a href="#">poprzedni</a>
+                  <a href="#">następny</a>
+                </div>
+              </div>
             </div>
             :
-            'Ładowanie'
+            <p>Ładowanie</p>
         }
-
-
       </div>
-
-
     )
   }
 }
